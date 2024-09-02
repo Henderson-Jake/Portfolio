2f424c6a9b0f73f37974ae288ec3d42094f4e4c0
Jake Henderson <henderson.jake.c@gmail.com>
2024-09-02 09:38:44 -0500

    Added React-Typed
    Added react-typed and a small typing animation to the intro component.

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './Intro.css';
+import { ReactTyped  } from "react-typed";
 //import img from '../port-img.png';
 
 
@@ -8,7 +9,17 @@ function Intro () {
         <div className="intro">
 
             <div className="intro-header">
-                <h2>Hey, I'm Jake</h2>
+                <h2>
+                    Hey, I'm{" "}
+                    <ReactTyped
+                        strings={["Jake", "a student", "a developer", "a problem solver"]}
+                        typeSpeed={100}
+                        loop
+                        backSpeed={20}
+                        cursorChar=">"
+                        showCursor={true}
+                    />
+                </h2>
             </div>
             <div className="intro-txt">
                 <p>I'm a student who can't get enough of the tech world. I'm constantly trying to learn all the latest and greatest developments in the tech space</p>
